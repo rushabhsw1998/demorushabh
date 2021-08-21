f1d2dfe87cbde27d9776e54a31333e533749ca35
rushabhsw1998 <rushabhsw1998@gmail.com>
2021-08-21 14:41:53 +0530

    Q3

---
+++ DemoRushabh/Q3_Rushabh_Waghmare.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRushabh
+{
+    class Q3_Rushabh_Waghmare
+    {
+       static string checkPath(string a)
+        {
+            int size = a.Length;
+            char[] ch = new char[a.Length];
+
+            Console.WriteLine("-");
+
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                ch[i] = a[i];
+            }
+            Console.WriteLine("-");
+
+            foreach (char c in ch)
+            {
+                if (c == Convert.ToChar("U"))
+                {
+                    Console.WriteLine("/");
+                    
+                }
+                else if(c == Convert.ToChar("D"))
+                {
+                    Console.WriteLine("//");
+                }
+                Console.WriteLine(c);
+            }
+            Console.WriteLine("-");
+         
+            return a;
+        }
+
+
+       public static void Main(string[] args)
+        {
+            string A = "UUDUD";
+            Console.WriteLine(checkPath(A));
+            Console.ReadLine();
+        }
+    }
+}
