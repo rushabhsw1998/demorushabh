f72c084fe6386b549527f503a6e2a5bb359ec0aa
rushabhsw1998 <rushabhsw1998@gmail.com>
2021-08-21 13:24:58 +0530

    initial commit

---
+++ DemoRushabh/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRushabh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 10;
+            
+            Console.WriteLine("Enter Number",+ j);
+            if (j > 0)
+            {
+                int result = j / 3;
+                Console.WriteLine(result);
+
+            }
+
+            else
+            {
+                Console.WriteLine("Please Provide a valid positive number.");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
